f6137334f00886e95ba05782578aa01181e1fb64
Henry <hengyuchou@microtest.com.tw>
2020-09-04 21:06:48 +0800

    [v1.0.1] Rename the project

--- CEPGP_DistributionFromBags.lua
+++ CEPGP_DistributionFromBags.lua
@@ -1,12 +1,12 @@
 --[[ Globals ]]--
 
-CEPGP_AFB_Addon = "CEPGP_AnnounceFromBag"
-CEPGP_AFB_LoadedAddon = false
-CEPGP_AFB_LastTime = 0
-CEPGP_AFB_LastLink = nil
+CEPGP_DFB_Addon = "CEPGP_DistributionFromBags"
+CEPGP_DFB_LoadedAddon = false
+CEPGP_DFB_LastTime = 0
+CEPGP_DFB_LastLink = nil
 
-SLASH_CEPGPAFB1 = "/CEPAFB"
-SLASH_CEPGPAFB2 = "/cepafb"
+SLASH_CEPGPDFB1 = "/CEPDFB"
+SLASH_CEPGPDFB2 = "/cepdfb"
 
 --[[ SAVED VARIABLES ]]--
 
@@ -15,81 +15,81 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("VARIABLES_LOADED")
 frame:RegisterEvent("ADDON_LOADED")
 
-function CEPGP_AFB_print(str, err)
+function CEPGP_DFB_print(str, err)
 	if not str then return; end;
 	if err == nil then
-		DEFAULT_CHAT_FRAME:AddMessage("|c00FFF569CEPGP_AFB: " .. tostring(str) .. "|r");
+		DEFAULT_CHAT_FRAME:AddMessage("|c00FFF569CEPGP_DFB: " .. tostring(str) .. "|r");
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("|c00FFF569CEPGP_AFB:|r " .. "|c00FF0000Error|r|c00FFF569 - " .. tostring(str) .. "|r");
+		DEFAULT_CHAT_FRAME:AddMessage("|c00FFF569CEPGP_DFB:|r " .. "|c00FF0000Error|r|c00FFF569 - " .. tostring(str) .. "|r");
 	end
 end
 -- [[ WOW API HOOK ]] --
 hooksecurefunc("HandleModifiedItemClick", function(link)
-	if CEPGP_AFB_LastLink == link then	-- too quickly
-		if time() - CEPGP_AFB_LastTime < 2 then
+	if CEPGP_DFB_LastLink == link then	-- too quickly
+		if time() - CEPGP_DFB_LastTime < 2 then
 			return
 		end
 	end
 
-	if CEPGP_AFB_frame:IsShown() and not ChatEdit_GetActiveWindow() then
+	if CEPGP_DFB_frame:IsShown() and not ChatEdit_GetActiveWindow() then
 		local _, itemLink = GetItemInfo(link)
 		if itemLink then
-			CEPGP_AFB_LootFrame_Update(itemLink)
+			CEPGP_DFB_LootFrame_Update(itemLink)
 		end
 
-		CEPGP_AFB_LastTime = time()
-		CEPGP_AFB_LastLink = link
+		CEPGP_DFB_LastTime = time()
+		CEPGP_DFB_LastLink = link
 	end
 end)
 
 -- [[ CORE ]] --
-local function CEPGP_AFB_SlashCmd(arg)
-	if not CEPGP_AFB_frame:IsShown()then
-		ShowUIPanel(CEPGP_AFB_frame)
+local function CEPGP_DFB_SlashCmd(arg)
+	if not CEPGP_DFB_frame:IsShown()then
+		ShowUIPanel(CEPGP_DFB_frame)
 		OpenAllBags()
 	else
-		HideUIPanel(CEPGP_AFB_frame)
+		HideUIPanel(CEPGP_DFB_frame)
 	end
 end
 
-SlashCmdList["CEPGPAFB"] = CEPGP_AFB_SlashCmd
+SlashCmdList["CEPGPDFB"] = CEPGP_DFB_SlashCmd
 
-function CEPGP_AFB_init()
+function CEPGP_DFB_init()
 	if (_G.CEPGP) then
 		_G.CEPGP_distribute_popup_give = CEPGP_distribute_popup_give_Hook
 	end
 
 	if (GetLocale() == "zhTW") then
-		_G["CEPGP_AFB_frame_text"]:SetText("Shift+Click 點擊包包裡的物品來開始喊裝\n\n分配完後要自行手動拿裝給人")
+		_G["CEPGP_DFB_frame_text"]:SetText("Shift+Click 點擊包包裡的物品來開始喊裝\n\n分配完後要自行手動拿裝給人")
 	else
-		_G["CEPGP_AFB_frame_text"]:SetText("Shift+Click an item in inventory to start the distribution process.\n\nAt the end, you need to give the item to the winner MANUALLY.")
+		_G["CEPGP_DFB_frame_text"]:SetText("Shift+Click an item in inventory to start loot distribution process.\n\nAt the end, you need to give the item to the winner MANUALLY.")
 	end
 end
 
 local function OnEvent(self, event, arg1, arg2)
-    if arg1 ~= CEPGP_AFB_Addon then return end
-	if event == "ADDON_LOADED" and CEPGP_AFB_LoadedAddon == false then
+    if arg1 ~= CEPGP_DFB_Addon then return end
+	if event == "ADDON_LOADED" and CEPGP_DFB_LoadedAddon == false then
         self:UnregisterEvent("ADDON_LOADED")
-        CEPGP_AFB_LoadedAddon = true
-		CEPGP_AFB_init()
+        CEPGP_DFB_LoadedAddon = true
+		CEPGP_DFB_init()
     end
 end
 
 frame:SetScript("OnEvent", OnEvent)
 
-function CEPGP_AFB_LootFrame_Update(itemLink)
+function CEPGP_DFB_LootFrame_Update(itemLink)
 	if GetLootMethod() ~= "master" then
 		if (GetLocale() == "zhTW") then
-			CEPGP_AFB_print("不在隊長分配模式", 1);
+			CEPGP_DFB_print("不在隊長分配模式", 1);
 		else
-			CEPGP_AFB_print("The loot method is not Master Looter", 1);
+			CEPGP_DFB_print("The loot method is not Master Looter", 1);
 		end
 		return
 	elseif CEPGP_isML() ~= 0 then
 		if (GetLocale() == "zhTW") then
-			CEPGP_AFB_print("你不是分裝者.", 1);
+			CEPGP_DFB_print("你不是分裝者.", 1);
 		else
-			CEPGP_AFB_print("You are not the Loot Master.", 1);
+			CEPGP_DFB_print("You are not the Loot Master.", 1);
 		end
 		return
 	end
@@ -120,14 +120,14 @@ end
 
 -- [[ CEPGP HOOK ]] --
 function CEPGP_distribute_popup_give_Hook()
-	if not CEPGP_AFB_frame:IsShown() then
+	if not CEPGP_DFB_frame:IsShown() then
 		for i = 1, 40 do
 			if GetMasterLootCandidate(CEPGP_lootSlot, i) == CEPGP_distPlayer then
 				GiveMasterLoot(CEPGP_lootSlot, i);
 				return;
 			end
 		end
-		CEPGP_AFB_print(CEPGP_distPlayer .. " is not on the candidate list for loot", true);
+		CEPGP_DFB_print(CEPGP_distPlayer .. " is not on the candidate list for loot", true);
 		CEPGP_distPlayer = "";
 		
 	else
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# CEPGP AnnounceFromBag
+# CEPGP Distribution From Bags
 
-CEPGP AnnounceFromBag is a plugin of CEPGP addon.\
-The Loot Master can announce items and distribution calls for the items from inventory.\
+CEPGP DistributionFromBags is a plugin of CEPGP addon.\
+The Loot Master can start loot distribution process from the inventory.\
 This would allow the Loot Master to grab all items on all bosses, and make distribution at the end of the raid.\
 When the "bid" part is finished, the Loot Master need to give items manually.
 
 # How to use
 
-1. Type /cepafb to show up the window
+1. Type /cepdfb to show up the window
 2. When the window is on screen, Shift+Click an item in inventory to start the distribution process.
 3. Click on the person with the highest PR, choose the reason for distrubition, and record the winner's GP points.
 4. Manually give the item to the winner.
@@ -16,14 +16,14 @@ Compatible with CEPGP version 1.12.25
 
 ##
 
-CEPGP AnnounceFromBag 是 CEPGP addon 的 plugin。\
+CEPGP DistributionFromBags 是 CEPGP addon 的 plugin。\
 分裝者可以直接點選包包裡的物品來喊裝。 \
 可以收集所有掉落物，等到出團結束後統一分配裝備。\
 喊裝完成，記錄好GP點數後，分裝者要手動交易物品給獲勝者。
 
 # 使用方式
 
-1. 打 /cepafb 叫出視窗
+1. 打 /cepdfb 叫出視窗
 2. 在視窗開啟的情況下，Shift+Click 點擊包包裡的裝備，開始進行喊裝流程
 3. 點選PR最高的人，選擇給裝的理由，紀錄獲勝者的GP點數
 4. 手動拿裝給獲勝者

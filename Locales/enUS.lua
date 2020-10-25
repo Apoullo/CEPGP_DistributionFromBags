89a8196afab6b3820b8a9aa6d5aab47846fd740f
Henry <hengyuchou@microtest.com.tw>
2020-10-26 00:20:52 +0800

    [v1.13.12] Changes: - Instructions.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,7 +1,7 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("CEPGP_DistributionFromBags", "enUS", true)
 if not L then return end
 
-L["Instruction"] = "CLICK an item in inventory to start loot distribution process.\n\nAt the end, you need to give the item to the winner MANUALLY."
+L["Instruction"] = "Left-Click an item in inventory to start loot distribution process.\n\nAt the end, you need to give the item to the winner MANUALLY."
 L["The loot method is not Master Looter"] = true
 L["You are not the Loot Master"] = true
 L[" is the winner. Come to trade with me"] = true
--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,7 +1,7 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("CEPGP_DistributionFromBags", "zhCN")
 if not L then return end
 
-L["Instruction"] = "CLICK 点击包包里的物品来开始喊装\n\n分配完后要自行手动拿装给人"
+L["Instruction"] = "左键点击包包里的物品来开始喊装\n\n分配完后要自行手动拿装给人"
 L["The loot method is not Master Looter"] = "不在队长分配模式"
 L["You are not the Loot Master"] = "你不是装备分配者"
 L[" is the winner. Come to trade with me"] = " 是赢家，请过来跟我交易"

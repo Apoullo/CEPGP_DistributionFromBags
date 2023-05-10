69ddf23552bbb23fd48e603f5b807b679485c39c
Erlan Kaparov <NooBiToo@users.noreply.github.com>
2023-05-10 16:27:49 +0800

    Create ruRU.lua

---
+++ Locales/ruRU.lua
@@ -0,0 +1,10 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CEPGP_DistributionFromBags", "enUS", true)
+if not L then return end
+
+L["Instruction"] = "Перетащите предмет в инвентаре с зажатой клавишей Shift, чтобы начать процесс распределения добычи.\n\nВ конце вы должны вручную передать предмет победителю."
+L["The loot method is not Master Looter"] = "Метод добычи не является Мастер-лутером"
+L["You are not the Loot Master"] = "Вы не являетесь Мастер-лутером"
+L[" is the winner. Come to trade with me"] = " - победитель. Подойдите ко мне для обмена."
+L[" is the winner"] = " - победитель.\n\nНажмите кнопку [Торговля] рядом с победителем."
+L["Wrong Item"] = " не может получить этот предмет."
+L["Give GP without Trade confirm"] = "Выдавать GP без подтверждения обмена\n(Если отмечено, может произойти ситуация, когда GP будет выдан тому, кто не может получить добычу)"
